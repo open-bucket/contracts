be4327e4730199a61f35460a37ca324bd9f0894b
Hung Vu <vunguyenhung@outlook.com>
2018-07-06 15:51:44 +0700

    Finish Consumer contract & add test

--- contracts/Consumer.sol
+++ contracts/Consumer.sol
@@ -2,21 +2,16 @@ pragma solidity ^0.4.23;
 
 contract Consumer {
 
-    struct Serve {
-        address producer;
-        uint servedBytes;
-    }
-
     mapping(address => uint) balances;
     address public consumer;
     address public tracker;
+    uint public weiPerByteServed = 100000; // 1 GB = 100 Szabo = $0.05 - Based on current Ether price: ~ $500/Ether
+    uint public weiPerByteStored = 30000;  // 1 GB = 30 Szabo = $0.015 - Based on current Ether price: ~ $500/Ether
 
-    // consumer only has consumerId here.
-    // balance of consumer
-    // this don't need consumerId
     constructor(address _consumer, address _tracker) public payable {
         consumer = _consumer;
         tracker = _tracker;
+        balances[consumer] = msg.value;
     }
 
     modifier onlyTracker() {
@@ -24,78 +19,49 @@ contract Consumer {
         _;
     }
 
-//    function transfer(Serve serve, uint priceEachByte) internal
-//    returns (bool success) {
-//        // 100000 wei for each served byte
-//        uint payment = priceEachByte * serve.servedBytes;
-//        balances[address(serve.producer)] += payment;
-//        // User can send money to the contract by any address
-//        // But can only withdraw from 1 address ?
-//        // TODO: save the address that activate the consumer to DB -> add field address to Consumers
-//
-//        // must be the address
-//        // Don't let consumer withdraw Either
-//        // money withdraw = address(this).balance
-//        // client must registerAddress() to the consumer contract to withdraw the money from that address only
-//        // Client must call a function to destroy the contract when they want to withdraw the money ?
-//        // can't loop through the mapping
-//        //        balances[consumerId];
-//        // subtract consumer balance
-//    }
+    modifier balanceExistsOrNotEmpty() {
+        require(balances[msg.sender] != 0, "Balance is not exists or is empty.");
+        _;
+    }
 
-    // TODO: can't user Serve[] in param
-//    function served(Serve[] serves) public onlyTracker {
-//        for (uint i = 0; i < serves.length; i++) {
-//            // 100000 wei for each served byte
-//            uint payment = 100000 * serves[i].servedBytes;
-//            balances[address(serves[i].producer)] += payment;
-//        }
-//        // let client withdraw will let they pay the gas. Send to user let us pay the gas
-//    }
-//
-//    function stored(Serve[] serves) public onlyTracker {
-//        for (uint i = 0; i < serves.length; i++) {
-//            // 100000 wei for each served byte
-//            uint payment = 30000 * serves[i].servedBytes;
-//            balances[address(serves[i].producer)] += payment;
-//        }
-//        // price of stored 1 byte = 30000 wei
-//        //        producerAddress.transfer(storedBytes * 30000);
-//    }
+    function transfer(address producer, uint servedBytes, uint weiPerByte) internal
+    {
+        // Assuming current contract's balance is sufficient
+        uint paymentAmount = weiPerByte * servedBytes;
+        balances[producer] += paymentAmount;
+        balances[consumer] -= paymentAmount;
+    }
 
+    function transferMany(address[] producerArr, uint[] servedBytesArr, uint weiPerByte) internal
+    {
+        for (uint i = 0; i < producerArr.length; i++) {
+            transfer(producerArr[i], servedBytesArr[i], weiPerByte);
+        }
+    }
 
-    // we know which producer serve which part & size of the shard
-    // When client downloaded the file, assemble the shard & decrypt the file
-    // Then Client calculate the md5 hash of the file,
-    // -> client send request downloaded with the hash of downloaded file.
-    // Tracker check the hash & pay
-    //  If the hash is valid: Tracker call to Consumer contract to pay all the producer based on
-    // total size of the shards of the file they keep
-    // {producerAddress: 0x121111, servedBytes: totalShardsSize}
-    // we don't need tier in this contract, since when many producer serves it,consumer must pay for as many producers
-    // consider do withdraw instead of transfer
+    function serves(address[] producerArr, uint[] servedBytesArr) public onlyTracker {
+        transferMany(producerArr, servedBytesArr, weiPerByteServed);
+    }
 
+    function stores(address[] producerArr, uint[] servedBytesArr) public onlyTracker {
+        transferMany(producerArr, servedBytesArr, weiPerByteStored);
+    }
 
+    function withdraw()
+    public
+    balanceExistsOrNotEmpty
+    {
+        uint returnValue = balances[msg.sender];
+        delete balances[msg.sender];
 
-    // this contract doesn't need to store the producers
-    // since they change constantly
-    // value will be calculated by GB Downloaded. 1 GB = $0.05 = 100 szabo => 1 byte = 100000 Wei = 100 kwei
+        msg.sender.transfer(returnValue);
+    }
 
-    //     value = byteServed * 100 kwei
-    // to keep it simple, tracker will call function served(producerAddress, byteServed)
-    // Office price: 1 byte = 100 kwei
-
-    //     served(producerAddress, byte) onlyTracker {
-    //         producerAddress.transfer(byte * 100000 wei)
-    //    }
-
-    // stored(producerAddress, byte) onlyTracker {
-    //     producerAddress.transfer(byte * 30000 wei)
-    //}
-
-    // withdraw() onlyOwner {
-    //     msg.sender.transfer(address(this).balance)
-    //}
-
-    // destroy the contract ?
+    function getBalance(address user)
+    public
+    view
+    returns (uint)
+    {
+        return balances[user];
+    }
 }
